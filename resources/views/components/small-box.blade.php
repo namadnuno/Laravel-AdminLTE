326b754f5271be2bf4aa280189fedb1bfa9b9336
Nuno Alexandre <nunnomalex@gmail.com>
2018-02-03 21:36:26 +0000

    added an small-box component

---
+++ resources/views/components/small-box.blade.php
@@ -0,0 +1,18 @@
+<div class="small-box bg-aqua">
+    <div class="inner">
+        <h3>{{ $count }}</h3>
+
+        <p>{{ $text }}</p>
+    </div>
+    <div class="icon">
+        {{ $icon }}
+    </div>
+    <a href="#" class="small-box-footer">
+        @isset ($more_info_text)
+            {{ $more_info_text }}
+        @endisset
+        @unless ($more_info_text)
+            More info
+        @endunless  <i class="fa fa-arrow-circle-right"></i>
+    </a>
+</div>
