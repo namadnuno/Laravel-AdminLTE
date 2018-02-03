1ef44a242d2f843803a537baa6816c2fc0acec25
Nuno Alexandre <nunnomalex@gmail.com>
2018-02-03 21:35:29 +0000

    isset to bg on info-box

--- resources/views/components/info-box.blade.php
+++ resources/views/components/info-box.blade.php
@@ -1,5 +1,7 @@
 <div class="info-box">
-    <span class="info-box-icon {{ $bg }}">{{ $icon }}</span>
+    <span class="info-box-icon @isset ($bg)
+        {{ $bg }}
+    @endisset ">{{ $icon }}</span>
     <div class="info-box-content">
         <span class="info-box-text">{{ $text }}</span>
         <span class="info-box-number">{{ $count }}</span>
