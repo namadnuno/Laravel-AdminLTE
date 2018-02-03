c2d1d938fba4274945d6c7fdfca34f68d0ff511f
Nuno Alexandre <nunnomalex@gmail.com>
2018-02-03 21:28:25 +0000

    added an info-box component

---
+++ resources/views/components/info-box.blade.php
@@ -0,0 +1,7 @@
+<div class="info-box">
+    <span class="info-box-icon {{ $bg }}">{{ $icon }}</span>
+    <div class="info-box-content">
+        <span class="info-box-text">{{ $text }}</span>
+        <span class="info-box-number">{{ $count }}</span>
+    </div>
+</div>
